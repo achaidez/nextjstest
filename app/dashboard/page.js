8c07650bca71d0c76a15bda064bd3ea5823d0f12
achaidez <chaidez62@gmail.com>
2024-02-02 11:22:56 -0700

    changed links to open in same tab instead of a new one

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -6,9 +6,9 @@ export default function Page() {
       <h1 className={`mb-3 text-3xl font-bold`}>Hello, Dashboard Page!</h1>
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
-            href="/input"
+            href="/dashboard/input"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
+            target="_self"
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
