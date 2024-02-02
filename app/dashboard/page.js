e70b6ee16b5533fbe845b5af511f9161981b3212
achaidez <chaidez62@gmail.com>
2024-02-02 10:53:49 -0700

    Added dashboard and input pages and linked dashboard to input page

---
+++ app/dashboard/page.js
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+// `app/dashboard/page.js` is the UI for the `/dashboard` URL
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className={`mb-3 text-3xl font-bold`}>Hello, Dashboard Page!</h1>
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <a
+            href="/input"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Input{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Track Here
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Output{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Track Here
+            </p>
+          </a>
+        </div>
+    </main>
+  )
+}
---
+++ app/input/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className={`mb-3 text-3xl font-bold`}>Hello, Input page!</h1>
+    </main>
+  )
+}
